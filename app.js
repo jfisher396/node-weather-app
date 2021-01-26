297fb4afb77818796ef1c39e76be2f8e7f4670ba
James Fisher <jfisher396@hotmail.com>
2021-01-26 15:15:47 -0800

    revised API response log

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const request = require('request');
 
-const url = process.env.API_URL + "query=37.8267,-122.4233";
+const url = process.env.API_URL + "query=Seattle&units=f";
 
 request({ url: url, json: true }, (err, res) => {
     // console.log(res.body.current);
-    console.log(`It is currently ${res.body.current.temperature} degrees. It feels like ${res.body.current.feelslike} degrees out.`)
+    console.log(`It is currently ${res.body.current.weather_descriptions[0]} with a temperature of ${res.body.current.temperature} degrees. It feels like ${res.body.current.feelslike} degrees out.`)
 })
 
 
