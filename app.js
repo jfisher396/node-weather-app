e3cd5830e9531168737305262ab75042e0bbfc62
James Fisher <jfisher396@hotmail.com>
2021-01-26 13:47:03 -0800

    added url to weatherstack api

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+require("dotenv").config();
 const request = require('request');
+
+
+const url = process.env.API_URL;
+
+request({ url: url }, (err, res) => {
+    const data = JSON.parse(res.body);
+    console.log(data.current)
+})
