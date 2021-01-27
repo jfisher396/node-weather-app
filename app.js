b14e2f20864a129ffd2f2ef320c8ff16257360f4
James Fisher <jfisher396@hotmail.com>
2021-01-26 16:00:56 -0800

    linked in the mapbox api

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const request = require('request');
 
-const url = process.env.API_URL + "query=Seattle&units=f";
+const weatherstackUrl = process.env.WEATHERSTACK_API_URL + "query=Seattle&units=f";
+const mapboxUrl = process.env.MAPBOX_API_URL;
 
-request({ url: url, json: true }, (err, res) => {
+
+request({ url: weatherstackUrl, json: true }, (err, res) => {
     // console.log(res.body.current);
     console.log(`It is currently ${res.body.current.weather_descriptions[0]} with a temperature of ${res.body.current.temperature} degrees. It feels like ${res.body.current.feelslike} degrees out.`)
 })
 
-
+request({ url: mapboxUrl, json: true }, (err, res) => {
+    const latitude = res.body.features[0].center[1];
+    const longitude = res.body.features[0].center[0];
+    console.log(`The longitude is ${longitude} and the latitude is ${latitude}`);
+})
