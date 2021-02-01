4fd330bd8d37c63352b0a9d5b6b545f3ffbf543e
James Fisher <jfisher396@hotmail.com>
2021-02-01 10:26:21 -0800

    added error handling to mapbox API

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 require("dotenv").config();
 const request = require('request');
 
-const weatherstackUrl = process.env.WEATHERSTACK_API_URL + "query=Seattle&units=f";
+// const weatherstackUrl = process.env.WEATHERSTACK_API_URL + "query=Seattle&units=f";
 
-// const mapboxUrl = process.env.MAPBOX_API_URL;
+const mapboxUrl = process.env.MAPBOX_API_URL;
 
 
-request({ url: weatherstackUrl, json: true }, (err, res) => {
+// request({ url: weatherstackUrl, json: true }, (err, res) => {
+//     if (err) {
+//         console.log('Unable to connect to WeatherStack API')
+//     } else if (res.body.error) {
+//         console.log(res.body.error)
+//     } else {
+//         console.log(`It is currently ${res.body.current.weather_descriptions[0]} with a temperature of ${res.body.current.temperature} degrees. It feels like ${res.body.current.feelslike} degrees out.`)
+//     }
+    
+// })
+
+request({ url: mapboxUrl, json: true }, (err, res) => {
+    
     if (err) {
-        console.log('Unable to connect to WeatherStack API')
-    } else if (res.body.error) {
-        console.log(res.body.error)
+        console.log('Unable to connect to MapBox API')
+    } else if (res.body.features.length === 0) {
+        console.log('No coordinates found for that search query.  Please try another.')
     } else {
-        console.log(`It is currently ${res.body.current.weather_descriptions[0]} with a temperature of ${res.body.current.temperature} degrees. It feels like ${res.body.current.feelslike} degrees out.`)
+        const latitude = res.body.features[0].center[1];
+        const longitude = res.body.features[0].center[0];
+        console.log(`The longitude is ${longitude} and the latitude is ${latitude}`);
     }
     
 })
-
-// request({ url: mapboxUrl, json: true }, (err, res) => {
-//     const latitude = res.body.features[0].center[1];
-//     const longitude = res.body.features[0].center[0];
-//     console.log(`The longitude is ${longitude} and the latitude is ${latitude}`);
-// })
