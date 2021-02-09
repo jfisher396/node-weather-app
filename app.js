4639b68bca350ca875c90ac01c6d36531dcee31b
James Fisher <jfisher396@hotmail.com>
2021-02-09 09:43:59 -0800

    created geocode file in utils folder

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const request = require('request');
+const geocode = require('./utils/geocode');
 
 // const weatherstackUrl = process.env.WEATHERSTACK_API_URL + "query=Seattle&units=f";
 
-const mapboxUrl = process.env.MAPBOX_API_URL;
+// const mapboxUrl = process.env.MAPBOX_API_URL;
 
 
 // request({ url: weatherstackUrl, json: true }, (err, res) => {
@@ -17,16 +18,21 @@ const mapboxUrl = process.env.MAPBOX_API_URL;
     
 // })
 
-request({ url: mapboxUrl, json: true }, (err, res) => {
+// request({ url: mapboxUrl, json: true }, (err, res) => {
     
-    if (err) {
-        console.log('Unable to connect to MapBox API')
-    } else if (res.body.features.length === 0) {
-        console.log('No coordinates found for that search query.  Please try another.')
-    } else {
-        const latitude = res.body.features[0].center[1];
-        const longitude = res.body.features[0].center[0];
-        console.log(`The longitude is ${longitude} and the latitude is ${latitude}`);
-    }
+//     if (err) {
+//         console.log('Unable to connect to MapBox API')
+//     } else if (res.body.features.length === 0) {
+//         console.log('No coordinates found for that search query.  Please try another.')
+//     } else {
+//         const latitude = res.body.features[0].center[1];
+//         const longitude = res.body.features[0].center[0];
+//         console.log(`The longitude is ${longitude} and the latitude is ${latitude}`);
+//     }
     
+// })
+
+geocode("Green Bay", (err, data) => {
+    console.log("Error", err);
+    console.log("Data", data)
 })
---
+++ utils/geocode.js
@@ -0,0 +1,27 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    process.env.MAPBOX_API_URL +
+    encodeURIComponent(address) +
+    process.env.MAPBOX_API_KEY;
+
+  request({ url: url, json: true }, (err, res) => {
+    if (err) {
+      callback("Unable to connect to MapBox API.", undefined);
+    } else if (res.body.features.length === 0) {
+      callback(
+        "No coordinates found for that search query.  Please try another.",
+        undefined
+      );
+    } else {
+      callback(undefined, {
+        latitude: res.body.features[0].center[1],
+        longitude: res.body.features[0].center[0],
+        location: res.body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
