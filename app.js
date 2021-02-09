6c2cadc1da1e33267750b98b719c60e9ca410d48
James Fisher <jfisher396@hotmail.com>
2021-02-09 11:34:03 -0800

    variable created for user input of city name

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const request = require("request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-// geocode function requires 2 arguments: a city name and a function
-geocode("Berlin", (err, data) => {
-  console.log("Error", err);
-  console.log("Data", data);
-  // location function requires 3 arguments: latitude, longitude and a function
-  forecast(40.7831, -73.9712, (err, data) => {
-    console.log("Error", err);
-    console.log("Data", data);
+const city = process.argv[2];
+
+// geocode function requires 2 arguments: a city name and a callback function
+geocode(city, (err, geocodeData) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  // forecast function requires 3 arguments: latitude, longitude and a callback function
+  forecast(geocodeData.latitude, geocodeData.longitude, (err, forecastData) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log(geocodeData.location);
+    console.log(forecastData)
   });
 });
