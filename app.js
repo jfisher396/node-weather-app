0035ffc4d0e2d0890c9454513a46976cd30f9af6
James Fisher <jfisher396@hotmail.com>
2021-02-09 11:00:52 -0800

    moved forecast function into geocode function

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 require("dotenv").config();
-const request = require('request');
-const geocode = require('./utils/geocode');
-const location = require('./utils/forecast');
-
+const request = require("request");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 // geocode function requires 2 arguments: a city name and a function
 geocode("Berlin", (err, data) => {
+  console.log("Error", err);
+  console.log("Data", data);
+  // location function requires 3 arguments: latitude, longitude and a function
+  forecast(40.7831, -73.9712, (err, data) => {
     console.log("Error", err);
-    console.log("Data", data)
-})
-
-// geocode function requires 3 arguments: latitude, longitude and a function
-location(40.7831,-73.9712, (err, data) => {
-    console.log("Error", err);
-    console.log("Data", data)
-})
+    console.log("Data", data);
+  });
+});
